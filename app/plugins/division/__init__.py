32d051da311bd57155aba996e9587f76cc8b67fc
Akarsh0809 <157727440+Akarsh0809@users.noreply.github.com>
2024-03-30 02:09:43 -0400

    Update __init__.py

--- app/plugins/division/__init__.py
+++ app/plugins/division/__init__.py
@@ -3,10 +3,10 @@ from app.commands import Command
 class DivisionCommand(Command):
     def execute(self, args):
         if args:
-            x = float(args[0])
-            y = float(args[1])
+            a = float(args[0])
+            b = float(args[1])
             try:
-                return x/y
+                return a/b
             except:
                 print ("error! user is trying to do division by zero.")
         else:
