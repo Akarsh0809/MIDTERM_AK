ed6137276e827d1984dc648e823d2ce98e85a4d1
Akarsh0809 <157727440+Akarsh0809@users.noreply.github.com>
2024-03-30 01:33:40 -0400

    Update __init__.py

--- app/plugins/addition/__init__.py
+++ app/plugins/addition/__init__.py
@@ -1,9 +1,22 @@
 from app.commands import Command
+
 class AdditionCommand(Command):
+    """
+    Command class for performing addition operation.
+    """
     def execute(self, args):
+        """
+        Execute the addition operation.
+
+        Args:
+            args (list): List of arguments containing numbers to add.
+
+        Returns:
+            float: Result of the addition operation.
+        """
         if args:
             a = float(args[0])
             b = float(args[1])
-            return a+b
+            return a + b
         else:
-            print ("nothing to add")
+            print("Nothing to add")
